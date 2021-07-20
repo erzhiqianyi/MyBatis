57d36c5dfd8ff29b2838f7bbe3ab6e328ef1381e
二之前一 <github@caofeng.me>
2021-07-21 00:06:59 +0800

    build read Environment

---
+++ JDBCDemo/src/test/java/me/caofeng/model/LoadUserJDBC.java
@@ -0,0 +1,50 @@
+package me.caofeng.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+class LoadUserJDBC {
+    @Test
+    public void loadUserByJdbc() throws ClassNotFoundException, SQLException {
+        String url = "jdbc:mysql://127.0.0.1:3306/mybatis?serverTimezone=UTC";
+        String userName = "root";
+        String password = "peak";
+
+        User userParam = new User();
+        userParam.setSchoolName("Sunny School");
+
+        //第一步: 加载驱动程序
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        //第二步: 获得数据库连接
+        Connection conn = DriverManager.getConnection(url, userName, password);
+
+        //第三步: 创建语句并执行
+        Statement statement = conn.createStatement();
+        ResultSet resultSet = statement.executeQuery(" select *  from `user` where schoolName = \'" + userParam.getSchoolName() + "\';");
+
+        //第四步: 处理数据库操作结果
+        List<User> userList = new ArrayList<>();
+        while (resultSet.next()) {
+            User user = new User();
+            user.setId(resultSet.getInt("id"));
+            user.setName(resultSet.getString("name"));
+            user.setEmail(resultSet.getString("email"));
+            user.setAge(resultSet.getInt("age"));
+            user.setSex(resultSet.getInt("sex"));
+            user.setSchoolName(resultSet.getString("schoolName"));
+            userList.add(user);
+        }
+
+        //第五步: 关闭连接
+        statement.close();
+        for (User user : userList) {
+            System.out.println("name : " + user.getName() + "; email : " + user.getEmail());
+        }
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # MyBatis
-Myabits read note and sample
+Myabits  source read note and sample
+
+## 源码阅读的意义
+### 为什么要阅读源码
+- 理解MyBatis实现原理
+- 学习优秀的的设计和技巧
+- 提示自我
+
+### 如何阅读源码
+- 调试追踪
+- 归类总结
+- 上下文整合
+- 合理利用工具
+
+### 为什么阅读MyBatis源码
+- MyBatis 应用广泛
+- 设计知识面广
+- 代码量合适
+
+## Mybatis概述
+### JDBC如何处理数据库操作
+- 连接流程  
+建立连接  → 拼装和执行sql语句 → 转化操作结果
+  [示例代码](JDBCDemo)
+### ORM框架
+对象关系映射(Object Relational Mapping ,ORM)
+
+### MyBatis特点
+- 核心映射
+  Java方法与SQL语句关联，SQL语句的参数或结果和对象关联，屏蔽关系型数据库，面向对象的方式进行数据库读写。
+- 缓存功能
+- 懒加载功能
+- 主键自增功能
+- 多数据集处理
+
+### MyBatis配置方式
+- 完全代码配置
+- 基于XMl配置
+- 外部框架配置
